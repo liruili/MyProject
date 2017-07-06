7a55695306c861d9e3e4fd1ce3cf43feec2c2608
LR <1204326279@qq.com>
2017-07-06 14:03:11 +0800

    Bill修改了MainActivity

--- SourcetreeTest/app/src/main/java/com/example/lirui/sourcetreetest/MainActivity.java
+++ SourcetreeTest/app/src/main/java/com/example/lirui/sourcetreetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lirui.sourcetreetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Bill修改代码
+        Log.d("tag","我是Bill");
     }
 }
